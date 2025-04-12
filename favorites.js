fa7686429e953e35ec2b02e01a725e63c8d81d49
kgrebets <kostyantyn.grebets@gmail.com>
2025-04-12 20:06:30 +0000

    Deploying to gh-pages from @ kgrebets/sport_energy@3011a94a034e7d289f8791885efaa011e4996a3f 🚀

--- favorites.js
+++ favorites.js
@@ -1,11 +1,11 @@
-import{c as m,d as c,Y as p,f as y,i as f,b as h,F as x}from"./assets/modal-controller-BN1Pmi3B.js";import"./assets/vendor-BAoYjrW1.js";function b(e){const a=m(e.name);return`
+import{c as m,d as n,Y as p,f as y,i as f,b as h,F as x}from"./assets/modal-controller-DxepiLtK.js";import"./assets/vendor-BAoYjrW1.js";function b(e){const r=m(e.name),a="/sport_energy/img/sprite.svg";return`
 <li class="exercises-category-tile-item">
                   <div class="exercises-category-tile-top">
                     <div class="exercises-category-tile-top-wrapper">
                     <span class="exercises-category-tile-badge">WORKOUT</span>
                     <button class="exercises-category-tile-button-delete" aria-label="Remove from favorites" data-id="${e._id}" data-modal-ignore>
                       <svg class="icon" width="20" height="20">
-                        <use href="./img/sprite.svg#trash"></use>
+                        <use href="${a}#trash"></use>
                       </svg>
                     </button>
                     </div>
@@ -13,7 +13,7 @@ import{c as m,d as c,Y as p,f as y,i as f,b as h,F as x}from"./assets/modal-cont
                     <button class="exercises-category-tile-button-start" aria-label="Start workout" data-id="${e._id}">
                      <span class="exercises-category-tile-start">Start</span>
                       <svg class="icon" width="20" height="20">
-                        <use href="./img/sprite.svg#arrow-right"></use>
+                        <use href="${a}#arrow-right"></use>
                       </svg>
                     </button>
                     </div>
@@ -23,10 +23,10 @@ import{c as m,d as c,Y as p,f as y,i as f,b as h,F as x}from"./assets/modal-cont
                   <div class="exercises-category-tile-middle">
                     <div class="exercises-category-tile-man-icon-container">
                       <svg class="exercises-category-tile-man-icon" width="24" height="24">
-                        <use href="./img/sprite.svg#running"></use>
+                        <use href="${a}#running"></use>
                       </svg>
                     </div>
-                    <h3 class="exercise-name">${a}</h3>
+                    <h3 class="exercise-name">${r}</h3>
                   </div>
 
                   <div class="exercises-category-tile-bottom">
@@ -44,9 +44,9 @@ import{c as m,d as c,Y as p,f as y,i as f,b as h,F as x}from"./assets/modal-cont
                     </div>
                   </div>
                 </li>
-    `}function l(){const e=document.querySelector(".exercises-content");if(!e)return;async function a(s){try{const t=await fetch(`${p}exercises/${s}`);if(!t.ok)throw new Error("Exercise not found");return await t.json()}catch(t){return console.error(`Failed to fetch exercise with id ${s}:`,t),null}}function d(s){c.removeFavorite(s)}function u(){e.querySelectorAll(".exercises-category-tile-button-delete").forEach(t=>{t.addEventListener("click",()=>{const i=t.dataset.id;i&&d(i)})})}async function v(){const s=c.getFavoriteIds();console.log(s);const t=s.map(r=>a(r)),o=(await Promise.all(t)).filter(r=>r!==null);if(o.length===0){e.innerHTML=`
+    `}function d(){const e=document.querySelector(".exercises-content");if(!e)return;async function r(s){try{const t=await fetch(`${p}exercises/${s}`);if(!t.ok)throw new Error("Exercise not found");return await t.json()}catch(t){return console.error(`Failed to fetch exercise with id ${s}:`,t),null}}function a(s){n.removeFavorite(s)}function u(){e.querySelectorAll(".exercises-category-tile-button-delete").forEach(t=>{t.addEventListener("click",()=>{const i=t.dataset.id;i&&a(i)})})}async function v(){const s=n.getFavoriteIds();console.log(s);const t=s.map(o=>r(o)),c=(await Promise.all(t)).filter(o=>o!==null);if(c.length===0){e.innerHTML=`
         <p class="no-favorites">
           It appears that you haven't added any exercises to your favorites yet.
           To get started, you can add exercises that you like to your favorites for easier access in the future.
-        </p>`;return}const g=o.map(b).join("");e.innerHTML=`<ul class="exercises-list">${g}</ul>`,u()}v()}y();l();f();const n=document.querySelector(".favorites-list");n instanceof HTMLElement&&h(n);document.addEventListener(x,()=>{l()});
+        </p>`;return}const g=c.map(b).join("");e.innerHTML=`<ul class="exercises-list">${g}</ul>`,u()}v()}y();d();f();const l=document.querySelector(".favorites-list");l instanceof HTMLElement&&h(l);document.addEventListener(x,()=>{d()});
 //# sourceMappingURL=favorites.js.map
